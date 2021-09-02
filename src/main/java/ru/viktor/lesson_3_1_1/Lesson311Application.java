48d8381eabcc5a34b02e732c16acde2673fde315
redy77 <redy@yandex.ru>
2021-09-02 17:28:24 +0300

    Starter-spring

---
+++ src/main/java/ru/viktor/lesson_3_1_1/Lesson311Application.java
@@ -0,0 +1,13 @@
+package ru.viktor.lesson_3_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson311Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/viktor/lesson_3_1_1/Lesson311ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.viktor.lesson_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
